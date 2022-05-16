4d198d59c6c07a133e9d7866e334463b6e4ac13b
MarioRemon <67578151+MarioRemon@users.noreply.github.com>
2022-05-16 21:29:34 +0200

    ha

---
+++ src/main/java/Withdraw.java
@@ -0,0 +1,8 @@
+public class Withdraw {
+    double withdraw(double withdraw, double balance){
+        if(balance >= withdraw)
+            return balance - withdraw;
+        else
+            return balance;
+    }
+}
